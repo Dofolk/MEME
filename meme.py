727ed1b5cdcb2aaa1b9ba5ff03f5603991fa3b4c
Dofolk <41058334+Dofolk@users.noreply.github.com>
2023-07-24 14:36:46 +0800

    Update meme.py

--- meme.py
+++ meme.py
@@ -4,6 +4,7 @@ import collections
 import random
 import math
 from math import sqrt
+from sklearn import preprocessing
 
 #%%
 #set up the global variables
@@ -23,21 +24,19 @@ class MEME:
         # the basic variables, parameters in the MEME
         # the symbols are corresponding to the article
         # the small z stores the information of big Z from motif for each subsequences and 
-        # the storage position is depending on the start position index of the subsequence
+          # the storage position is depending on the start position index of the subsequence
 
         self.Y = Y
         self.N = len(Y)
         self.l = [len(seqs) for seqs in Y]
         self.W = W
-        self.X, self.total_freq = self.data_div(Y, W)
+        self.X, self.total_freq, self.subseqs_amount_list = self.data_div(Y, W)
         self.n = len(self.X)
         self.Z = None
-        self.z = np.zeros([self.N, max(self.l)],dtype = 'float')
-        #self.z = [ [ [self.Z[idx][0] for idx in range(self.n)] for i in range(self.l[j])] for j in range(self.N)]
+        self.z = None
         
-        self.f = np.zeros((self.W, L))
-        self.f_0 = np.zeros((L, ))
-        #self.f = abs(np.random.rand())
+        self.f = None
+        self.f_0 = None
         
         self.theta1 = self.f
         self.theta2 = self.f_0
@@ -46,6 +45,7 @@ class MEME:
         self.lamb2 = None
         self.I = None
         self.I_str = None
+        self.I_subseq_freq = None
         self.var_init()
         self.theta = [self.theta1, self.theta2]
         self.lamb = [self.lamb1, self.lamb2]
@@ -53,16 +53,28 @@ class MEME:
     
     def var_init(self):
         # initialize the variables, parameters
-        # it will initialize: lambda 1, lambda 2, indicator function I(k,a)
+        # it will initialize: big Z, small z, lambda 1, lambda 2, f, f_0
 
         self.Z = np.repeat(np.array([[1,0]]), self.n, axis = 0)
+        if self.n != sum(self.subseqs_amount_list):
+            print('Error')
+        else:
+            self.z = list()
+            z_idx = 0
+            for i in range(self.n):
+                for j in self.subseqs_amount_list:
+                    self.z[i][j] = self.Z[z_idx][0]
+                    z_idx += 1
+            for i in range(len(self.z)):
+                self.z[i] = preprocessing.normalize(self.z[i])
+                
 
         lamb_range_m = min(sqrt(self.N)/self.n, 1/(2*self.W))
         lamb_range_M = max(sqrt(self.N)/self.n, 1/(2*self.W))
         self.lamb1 = random.uniform(lamb_range_m, lamb_range_M)
         self.lamb2 = 1 - self.lamb1
         
-        self.I, self.I_str = self.indicator()
+        self.I, self.I_str, self.I_subseq_freq = self.indicator()
         
         idx = 0
         for seq in range(self.N):
@@ -70,6 +82,12 @@ class MEME:
             val = np.pad(np.array([self.Z[idx][0] for idx in range(idx, idx + length - self.W + 1, 1)])\
                          , (0, self.W - 1), mode = 'constant').reshape(length,)
             self.z[seq][0:length] = val
+        
+        f = np.array([random.uniform(0,1) for _ in range(L * self.W)]).reshape(self.W, L)
+        for pos in range(self.W):
+            f[pos] = preprocessing.normalize(f[pos])
+        self.f = f
+        self.f_0 = preprocessing.normalize(np.array([random.unifrom(0,1) for _ in range(L)]).reshape(1, L))
 
         return None
     
@@ -80,25 +98,38 @@ class MEME:
 
         X = list()
         C = dict( zip(A.keys(), [0]*L) )
+        amount = list()
         for i in range(len(Y)):
-            X += [ Y[i][j:j+W] for j in range( len(Y[i]) - W + 1) ]
+            subsequences = [ Y[i][j:j+W] for j in range( len(Y[i]) - W + 1) ]
+            X += subsequences
+            amount.append(len(subsequences))
             for key in C:
                 C[key] += Y[i].count(key)
 
-        return X, C
+        return X, C, amount
 
     def indicator(self):
         # contribute the indicator function matrix results
+        # I_str_idx 
+          # type : np.array
+          # size : n x W
+          # meaning : transfer each subsequences' letters into integers
+        # I_str_count
+          # type : list of dictionary
+          # size : n dict() elements for each elements contain counter info.
+          # meaning : counts each subsequences letters appearance
         
         I_total = np.zeros((self.n, self.W, L), dtype = 'int')
         I_str_idx = np.zeros((self.n, self.W), dtype = 'int')
+        I_str_count = list()
         
         for subseq in range(self.n):
             for pos in range(self.W):
                 I_total[subseq][pos][ A[self.X[subseq][pos] ] ] = 1
                 I_str_idx[subseq][pos] = A[self.X[subseq][pos] ]
+            I_str_count.append(collections.Counter(list(I_str_idx)))
 
-        return I_total, I_str_idx 
+        return I_total, I_str_idx, I_str_count
 
     def condi_distributions(self, theta):
         # calculate the conditional distribution p(Xi | theta_j)
@@ -134,6 +165,30 @@ class MEME:
         return E_val
     
     def M_step(self, Z):
+        lamb1 = 0
+        lamb2 = 0
+        
+        # update lambda value
+        for i in range(self.n):
+            lamb1 += Z[i][0]
+            lamb2 += Z[i][1]
+        lamb1 /= self.n
+        lamb2 /= self.n
+
+        # update theta value
+        c_0k = np.zeros([1,L])
+        c_jk = np.zeros([self.W, L])
+        erase = self.erase()
+        
+        for k in range(L):
+            for i in range(self.n):
+                freq = self.I_subseq_freq[i]
+                c_0k[k] += Z[i][1]*freq[k]
+                
+        
+        return
+    
+    def erase(self,Z):
         return
     
     def update_var(self, Z, theta, lamb):
